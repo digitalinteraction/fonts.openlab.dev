591a98ac65dfdc2f178ea413a19d8955327e418d
Rob Anderson <robanderson@hey.com>
2023-02-10 11:41:50 +0000

    tweak zip script

--- scripts/gen_zips.sh
+++ scripts/gen_zips.sh
@@ -5,7 +5,7 @@ FOLDERS=$(ls src)
 for DIR in $FOLDERS
 do
   pushd "src/$DIR"
-  zip --update "$DIR.zip" --recurse-paths --exclude "$DIR.zip" *
+  zip --update "$DIR.zip" --recurse-paths --exclude "$DIR.zip" $(ls | sort)
   unzip -l "$DIR.zip"
   popd
 done
