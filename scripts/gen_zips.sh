0936f9ba3671d67d4432d022b53941d763a360c4
Rob Anderson <robanderson@hey.com>
2023-02-10 11:33:34 +0000

    add playfair zip and fix zip script

--- scripts/gen_zips.sh
+++ scripts/gen_zips.sh
@@ -1,20 +1,11 @@
 #!/usr/bin/env sh
 
 FOLDERS=$(ls src)
-FOLDERS=playfair
 
 for DIR in $FOLDERS
 do
   pushd "src/$DIR"
-  zip \
-    --update \
-    --recurse-paths \
-    --include "*.woff" \
-    --include "*.woff2" \
-    --include "*.css" \
-    --include "*.md" \
-    --include "*.txt" \
-    "$DIR.zip" \
-    .
+  zip --update "$DIR.zip" --recurse-paths --exclude "$DIR.zip" *
+  unzip -l "$DIR.zip"
   popd
 done
