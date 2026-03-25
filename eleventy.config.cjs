b660a483cd2505d10a3f2af8b3dc714e08df3332
Rob Anderson <rob@andrsn.uk>
2026-03-25 11:32:10 +0000

    improve font picker

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,10 +1,20 @@
 const { eleventyAlembic } = require('@openlab/alembic/11ty')
 
 const fontPicker = `
-<label class="field">
-<span class="field-label">Pick your font!</span>
-<select id="fontPicker" name="font"></select>
-</label>
+<details>
+  <summary>Font picker</summary>
+  <cluster-layout space="s-1" justify="space-between">
+    <label class="field" for="font">
+      <span class="field-label">Font</span>
+      <select id="font" name="font"></select>
+    </label>
+
+    <label class="field" for="weight">
+      <span class="field-label">Font weight</span>
+      <input type="range" min="400" max="700" id="weight" name="weight">
+    </label>
+  </cluster-layout>
+</details>
 `
 
 module.exports = function (eleventyConfig) {
